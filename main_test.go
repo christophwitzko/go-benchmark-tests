109884ba260b4ba78ac344638e1d35505a457ab5
Christoph Witzko <github@christophwitzko.com>
2022-08-03 14:43:19 +0200

    feat: add v2

--- main_test.go
+++ main_test.go
@@ -2,47 +2,50 @@ package main
 
 import (
 	"fmt"
-	"strconv"
 	"strings"
 	"testing"
 )
+
+func split(input string, c byte) []string {
+	res := make([]string, 0)
+	for {
+		pos := strings.IndexByte(input, c)
+		if pos == -1 {
+			return append(res, input)
+		}
+		res = append(res, input[:pos])
+		input = input[pos+1:]
+	}
+}
 
 func BenchmarkSplit(b *testing.B) {
 	b.ReportAllocs()
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
-		strings.Split("A,B,C,D", ",")
+		split("A,B,C,D", ',')
 	}
 }
 
-func ValidateOrderNumber(orderNumber string) error {
-	parts := strings.Split(orderNumber, "-")
-	if len(parts) != 4 {
-		return fmt.Errorf("invalid order number: %s", orderNumber)
-	}
-	checksums := make([]int, 3)
+func ValidateOrderNumber2(orderNumber string) error {
+	checksums := make([]int32, 3)
+	onRest := orderNumber
 	for i := 0; i < 3; i++ {
-		sum := 0
-		for _, c := range strings.Split(parts[i], "") {
-			val, err := strconv.ParseInt(c, 10, 8)
-			if err != nil {
-				return err
-			}
-			sum += int(val)
+		foundDash := strings.IndexByte(onRest, '-')
+		if foundDash == -1 {
+			return fmt.Errorf("invalid order number: %s", orderNumber)
 		}
-		checksums[i] = sum % 10
-	}
-	validChecksums := strings.Split(parts[3], "")
-	if len(validChecksums) != 3 {
-		return fmt.Errorf("invalid checksum: %s", parts[3])
-	}
-	for i, c := range validChecksums {
-		val, err := strconv.ParseInt(c, 10, 8)
-		if err != nil {
-			return err
+		for _, c := range onRest[:foundDash] {
+			checksums[i] += c - '0'
 		}
-		if checksums[i] != int(val) {
-			return fmt.Errorf("invalid checksum: %s", parts[3])
+		checksums[i] %= 10
+		onRest = onRest[foundDash+1:]
+	}
+	if len(onRest) != 3 {
+		return fmt.Errorf("invalid checksum length: %s", onRest)
+	}
+	for i, c := range onRest {
+		if checksums[i] != c-'0' {
+			return fmt.Errorf("invalid checksum: %s", onRest)
 		}
 	}
 	return nil
@@ -52,7 +55,26 @@ func BenchmarkValidateOrderNumber(b *testing.B) {
 	b.ReportAllocs()
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
-		_ = ValidateOrderNumber("122-321-111-563")
+		_ = ValidateOrderNumber2("122-321-111-563")
+	}
+}
+
+func parseUint(s string) int {
+	ret := 0
+	pos := 1
+	for i := len(s) - 1; i >= 0; i-- {
+		if s[i] > '9' || s[i] < '0' {
+			return -1
+		}
+		ret += int(s[i]-'0') * pos
+		pos *= 10
+	}
+	return ret
+}
+
+func TestParseUint(t *testing.T) {
+	if parseUint("999") != 999 {
+		t.Fatal("parseUint failed")
 	}
 }
 
@@ -60,6 +82,6 @@ func BenchmarkParseUint(b *testing.B) {
 	b.ReportAllocs()
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
-		_, _ = strconv.ParseUint("999", 10, 32)
+		_ = parseUint("999")
 	}
 }
